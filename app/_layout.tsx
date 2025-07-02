1b3bcc41c8cc9b86accb441c18c6e3e164e9a0da
Luiz Henrique <luizhjramos@outlook.com>
2025-07-01 21:39:26 -0300

    refactor: remove app boilerplate

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,33 +1,23 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   if (!loaded) {
-    return null;
+    return null; // or a loading indicator
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(protected)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-        <Stack.Screen name="sign-in" />
-      </Stack>
+    <Stack>
+      <Stack.Screen name="(protected)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+      <Stack.Screen name="sign-in" />
       <StatusBar style="auto" />
-    </ThemeProvider>
+    </Stack>
   );
 }
